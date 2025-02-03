c183467a583790ccf9f7b4dbbec6a069d13f7de3
Jose Pernia <joseapc90@gmail.com>
2025-02-03 06:48:28 -0300

    v1.0.0 Folder Settings

---
+++ TodoList-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList-SwiftUI
+//
+//  Created by Jose Pernia on 2025-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList-SwiftUI/TodoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUIApp.swift
+//  TodoList-SwiftUI
+//
+//  Created by Jose Pernia on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
